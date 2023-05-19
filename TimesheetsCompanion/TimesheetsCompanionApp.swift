ca258af4675a381bba043199dbbfc4573269692d
Nick Black <49686365+nickblack26@users.noreply.github.com>
2023-05-19 18:00:35 -0500

    Initial Commit

---
+++ TimesheetsCompanion/TimesheetsCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesheetsCompanionApp.swift
+//  TimesheetsCompanion
+//
+//  Created by Nick on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct TimesheetsCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
