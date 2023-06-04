79e8c264be6837008e589151491f2c419bee7a2b
Nick Black <49686365+nickblack26@users.noreply.github.com>
2023-06-04 13:00:02 -0500

    sets screen depending on current tab

--- TimesheetsCompanion/ContentView.swift
+++ TimesheetsCompanion/ContentView.swift
@@ -8,19 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var navigation: AppNavigation
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            switch navigation.tab {
+				case .login: LoginView()
+				case .home: HomeView()
+				case .timesheets: TimesheetsListView()
+				case .jobcodes: JobcodesListView()
+				case .working: WorkingReportView()
+				case .timesheetEdit: TimesheetDetail(timesheet: $navigation.timesheet)
+            }
         }
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
